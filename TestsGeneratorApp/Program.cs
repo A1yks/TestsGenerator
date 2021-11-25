94d8957069a52aad99b05777eb81bdf617886e01
A1yks <kuftirev.alex@gmail.com>
2021-11-26 02:03:35 +0300

    initial commit

---
+++ TestsGeneratorApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using TestsGeneratorLibrary;
+
+namespace TestsGeneratorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var generator = TestsGenerator.CreateGenerator(builder =>
+            {
+                builder.MaxReadFiles = 10;
+                builder.MaxGenerateTests = 10;
+                builder.MaxWriteFiles = 10;
+            });
+            generator.GenerateTestsAsync(@"E:\labs2021\mpp\lab4\TestGenerator\TestsGeneratorApp\input",
+                @"E:\labs2021\mpp\lab4\TestGenerator\TestsGeneratorApp\output").Wait();
+        }
+    }
+}
---
+++ TestsGeneratorLibrary/TestsGeneratorBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TestsGeneratorLibrary
+{
+    public class TestsGeneratorBuilder
+    {
+        private int maxReadFiles = 5;
+
+        private int maxGenerateTests = 5;
+
+        private int maxWriteFiles = 5;
+
+        public int MaxReadFiles
+        {
+            set => maxReadFiles = value;
+        }
+
+        public int MaxGenerateTests
+        {
+            set => maxGenerateTests = value;
+        }
+
+        public int MaxWriteFiles
+        {
+            set => maxWriteFiles = value;
+        }
+
+        public TestsGenerator Build()
+        {
+            return new TestsGenerator(maxReadFiles, maxGenerateTests, maxWriteFiles);
+        }
+    }
+}
